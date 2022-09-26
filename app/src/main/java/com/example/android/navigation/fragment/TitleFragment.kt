a19b4f5c09dca941c6a3e938e1495eb8ab8e1635
IbrohimB <botirovibrohim@gmail.com>
2022-09-26 14:45:26 +0500

    Mirmuhsin plz check, issue

--- app/src/main/java/com/example/android/navigation/fragment/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/fragment/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.R
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
@@ -24,6 +25,9 @@ class TitleFragment : Fragment() {
 
         val binding: FragmentTitleBinding=DataBindingUtil.inflate(
                inflater, R.layout.fragment_title,container,false )
+        binding.playButton.setOnClickListener{view:View->
+            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment5)
+        }
         return binding.root
     }
 }

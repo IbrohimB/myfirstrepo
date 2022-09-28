e6c463e893c418bcbf1fc054b51e019b3b31dd2e
IbrohimB <botirovibrohim@gmail.com>
2022-09-28 18:24:36 +0500

    Fragmentlani bog'lash

--- app/src/main/java/com/example/android/navigation/fragment/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/fragment/TitleFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.R
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
@@ -25,9 +26,10 @@ class TitleFragment : Fragment() {
 
         val binding: FragmentTitleBinding=DataBindingUtil.inflate(
                inflater, R.layout.fragment_title,container,false )
-        binding.playButton.setOnClickListener{view:View->
-            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment5)
-        }
+        binding.playButton.setOnClickListener (
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment5)
+                )
+
         return binding.root
     }
 }

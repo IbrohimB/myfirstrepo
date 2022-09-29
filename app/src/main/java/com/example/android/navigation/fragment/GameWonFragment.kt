a2e7519b16ba6d12ec34a8e1259097228c1cdc00
IbrohimB <botirovibrohim@gmail.com>
2022-09-29 20:01:44 +0500

    Video #19 finished

--- app/src/main/java/com/example/android/navigation/fragment/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/fragment/GameWonFragment.kt
@@ -20,6 +20,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -28,14 +30,21 @@ import com.example.android.navigation.databinding.FragmentGameWonBinding
 
 
 class GameWonFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
+
         binding.nextMatchButton.setOnClickListener { view:View->
-            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+            view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
+        val args0 = arguments
+        val args=GameWonFragmentArgs.fromBundle(args0 ?: bundleOf())
+        Toast.makeText(context,
+        "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",
+        Toast.LENGTH_LONG).show()
         return binding.root
     }
 }

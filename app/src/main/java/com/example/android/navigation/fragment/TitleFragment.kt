90fd472a0517f923cd1f1e9c3572af0b4736bac3
IbrohimB <botirovibrohim@gmail.com>
2022-09-29 16:42:26 +0500

    before disaster

--- app/src/main/java/com/example/android/navigation/fragment/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/fragment/TitleFragment.kt
@@ -1,10 +1,8 @@
 package com.example.android.navigation.fragment
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
@@ -29,7 +27,12 @@ class TitleFragment : Fragment() {
         binding.playButton.setOnClickListener (
             Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment5)
                 )
+        setHasOptionsMenu(true)
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
